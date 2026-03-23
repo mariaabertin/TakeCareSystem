556f7c3aef3076ca83720c22b2662f80be161479
Bertinbertin <bertinmaria@sempreceub.com>
2026-03-23 15:20:49 -0300

    codigo

---
+++ src/classes.py
@@ -0,0 +1,49 @@
+class Sono:
+    def __init__(self, horasSono):
+        self.horasSono = horasSono
+
+    def get_horasSono(self):
+        return self.horasSono
+    def set_horasSono(self, newHrSono):
+        self.horasSono = newHrSono
+
+class Treino:
+    def __init__(self, fezExercicio):
+        self.fezExercicio = fezExercicio
+
+    def get_fezExercicio(self):
+        return self.fezExercicio
+    def set_fezExercicio(self, newFezExercicio):
+        self.fezExercicio = newFezExercicio
+
+class Alimentacao:
+    def __init__(self, notaAlimentacao):
+        self.notaAlimentacao = notaAlimentacao
+
+    def get_notaAlimentacao(self):
+        return self.notaAlimentacao
+    def set_notaAlimentacao(self, newNotaAlimentacao):
+        self.notaAlimentacao = newNotaAlimentacao
+
+class Hidratacao:
+    def __init__(self, qntAgua):
+        self.qntAgua = qntAgua
+
+    def get_qntAgua(self):
+        return self.qntAgua
+    def set_qntAgua(self, newQntAgua):
+        self.qntAgua = newQntAgua
+
+class Leitura:
+    def __init__(self, leuLivro, qntPag):
+        self.leuLivro = leuLivro
+        self.qntPag = qntPag
+
+    def get_leuLivro(self):
+        return self.leuLivro
+    def get_qntPag(self):
+        return self.qntPag
+    def set_leuLivro(self, newLeuLivro):
+        self.leuLivro = newLeuLivro
+    def set_qntPag(self, newQntPag):
+        self.qntPag = newQntPag
---
+++ src/main.py
@@ -0,0 +1,60 @@
+from asyncio import timeout
+
+from classes import Sono, Treino, Alimentacao, Hidratacao, Leitura
+
+print("\n" + "-"*50)
+print("Seja bem-vindo(a) ao TakeCare!")
+print("Onde o objetivo é o autocuidado e a manutenção do seu bem estar!")
+print("-"*50)
+
+
+hrSono = float(input("Quantas horas você dormiu? "))
+treinou = input("Fez exercício hoje? (s/n): ").lower() == 's'
+notaAli = float(input("Qual a nota da sua alimentação hoje (0-10)? "))
+agua = float(input("Quantos litros de água bebeu hoje? "))
+leuLivro = input("Leu algum livro hoje? (s/n): ").lower() == 's'
+if leuLivro:
+    pagina = int(input("Quantas páginas? "))
+else:
+    pagina = 0
+
+
+horasSono = Sono(hrSono)
+fezExercicio = Treino(treinou)
+notaAlimentacao = Alimentacao(notaAli)
+qntAgua = Hidratacao(agua)
+leitura = Leitura(leuLivro, pagina)
+
+
+score = 0
+if horasSono.get_horasSono() >= 7: score += 20
+else:
+    porcentamemSono = hrSono/7
+    score += porcentamemSono*20
+
+if fezExercicio.get_fezExercicio(): score += 20
+
+if notaAlimentacao.get_notaAlimentacao() >= 8: score += 20
+else:
+    porcentamemAli = notaAlimentacao.get_notaAlimentacao()/8
+    score += porcentamemAli*20
+
+if qntAgua.get_qntAgua() >= 2: score += 20
+
+
+if leitura.get_leuLivro(): score += 5
+if leitura.get_qntPag() >= 10: score += 15
+
+
+print("\n" + "-"*35)
+print(f"Seu Score de Autocuidado hoje é: {score:.2f}%")
+
+if score >= 80:
+    print("Parabéns, você está sendo sua prioridade!")
+elif 60<= score <= 79:
+    print("Tem dias turbulentos, mas você está no caminho! Se mantenha firme!")
+else:
+    print("Se priorizar é o primeiro passo para uma vida equilibrada e prazeroza!")
+
+print("\n" + "-"*35)
+print("Amanhã é um novo dia, perfeito para novas oportunidades, acredite em si mesmo!")
